6b5e647263e9379b6fbda078659c9bd29b6904dd
rain7night <rainnight@outlook.com>
2019-02-11 16:05:23 +0800

    live lock and join

--- src/main/java/waitNotify/WaitNotify.java
+++ src/main/java/waitNotify/WaitNotify.java
@@ -1,32 +1,10 @@
-package WaitNotify;
+package waitNotify;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-/**
- * 调用wait()、notify()以及notifyAll()时需要注意的细节，如下。
- * 1）使用wait()、notify()和notifyAll()时需要先对调用对象加锁。
- * 2）调用wait()方法后，线程状态由RUNNING变为WAITING，并将当前线程放置到对象的等待队列。
- * 3）notify()或notifyAll()方法调用后，等待线程依旧不会从wait()返回，需要调用notify()或
- * notifAll()的线程释放锁之后，等待线程才有机会从wait()返回。
- * 4）notify()方法将等待队列中的一个等待线程从等待队列中移到同步队列中，而notifyAll()
- * 方法则是将等待队列中所有的线程全部移到同步队列，被移动的线程状态由WAITING变为BLOCKED。
- * 5）从wait()方法返回的前提是获得了调用对象的锁。
- * <p>
- * 超时等待
- * // 对当前对象加锁
- * public synchronized Object get(long mills) throws InterruptedException {
- * long future = System.currentTimeMillis() + mills;
- * long remaining = mills;
- * // 当超时大于0并且result返回值不满足要求
- * while ((result == null) && remaining > 0) {
- * wait(remaining);
- * remaining = future - System.currentTimeMillis();
- * }
- * return result;
- * }
- */
+
 public class WaitNotify {
     static boolean flag = true;
     static Object lock = new Object();
@@ -41,9 +19,7 @@ public class WaitNotify {
 
     static class Wait implements Runnable {
         public void run() {
-            // 加锁，拥有lock的Monitor
             synchronized (lock) {
-                // 当条件不满足时，继续wait，同时释放了lock的锁
                 while (flag) {
                     try {
                         System.out.println(Thread.currentThread() + " flag is true. wait" + "@ "
@@ -52,7 +28,6 @@ public class WaitNotify {
                     } catch (InterruptedException e) {
                     }
                 }
-                // 条件满足时，完成工作
                 System.out.println(Thread.currentThread() + " flag is false. running" + "@ "
                         + new SimpleDateFormat("HH:mm:ss").format(new Date()));
             }
@@ -61,10 +36,7 @@ public class WaitNotify {
 
     static class Notify implements Runnable {
         public void run() {
-            // 加锁，拥有lock的Monitor
             synchronized (lock) {
-                // 获取lock的锁，然后进行通知，通知时不会释放lock的锁，
-                // 直到当前线程释放了lock后，WaitThread才能从wait方法中返回
                 System.out.println(Thread.currentThread() + " hold lock. notify @ "
                         + new SimpleDateFormat("HH:mm:ss").format(new Date()));
                 lock.notifyAll();
@@ -75,7 +47,6 @@ public class WaitNotify {
                     e.printStackTrace();
                 }
             }
-            // 再次加锁
             synchronized (lock) {
                 System.out.println(Thread.currentThread() + " hold lock again. sleep" + "@ "
                         + new SimpleDateFormat("HH:mm:ss").format(new Date()));
