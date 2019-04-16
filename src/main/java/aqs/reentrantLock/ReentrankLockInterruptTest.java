7485e79f38acc2ac9af39cfa2081041e8b4eaf5f
rain7night <rainnight@outlook.com>
2019-04-16 14:29:03 +0800

    reentrantlock interrupt test

--- src/main/java/aqs/reentrantLock/ReentrankLockInterruptTest.java
+++ src/main/java/aqs/reentrantLock/ReentrankLockInterruptTest.java
@@ -3,35 +3,43 @@ package aqs.reentrantLock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- *
+ *ReentrantLock的锁中断测试分析
  */
 public class ReentrankLockInterruptTest {
-    private static volatile ReentrantLockInterruptThread thread = new ReentrantLockInterruptThread();
+
+    private final static ReentrantLock reLock = new ReentrantLock();
 
     public static void main(String[] args) {
-
+        final ReentrantLockInterruptThread thread = new ReentrantLockInterruptThread();
         thread.start();
-        try {
-            Thread.sleep(1000*10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         thread.interrupt();
     }
+    /**
+     true
+     false
+     */
+    /**
+     true
+     java.lang.InterruptedException
+     at java.util.concurrent.locks.AbstractQueuedSynchronizer.acquireInterruptibly(AbstractQueuedSynchronizer.java:1220)
+     at java.util.concurrent.locks.ReentrantLock.lockInterruptibly(ReentrantLock.java:335)
+     at aqs.reentrantLock.ReentrankLockInterruptTest$ReentrantLockInterruptThread.run(ReentrankLockInterruptTest.java:32)
+     false
+     */
+
 
     static class ReentrantLockInterruptThread extends Thread {
-        private final ReentrantLock reLock = new ReentrantLock();
 
         public void run() {
-            System.out.println(Thread.interrupted());
-            reLock.lock();
-//            try {
-//                reLock.lockInterruptibly();
-//                System.out.println(Thread.interrupted());
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-            System.out.println(Thread.interrupted());
+            System.out.println(Thread.currentThread().isInterrupted());
+//            reLock.lock();
+            try {
+                reLock.lockInterruptibly();
+                System.out.println(Thread.interrupted());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().isInterrupted());
         }
     }
 }
