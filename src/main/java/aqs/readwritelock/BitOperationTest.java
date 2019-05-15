f604ca0262f226e9ecd30f675f6e6583332ebd82
rain7night <rainnight@outlook.com>
2019-05-15 11:04:35 +0800

    bit

--- src/main/java/aqs/readwritelock/BitOperationTest.java
+++ src/main/java/aqs/readwritelock/BitOperationTest.java
@@ -8,11 +8,13 @@ public class BitOperationTest {
         System.out.println("-1>>2=" + (b >> 2));
         System.out.println("1>>>2=" + (a >>> 2));
         System.out.println("-1>>>2=" + (b >>> 2));
+        System.out.println("1<<6=" + (1 << 6));//2的6次方
     }
     /**
      1>>2=0
      -1>>2=-1
      1>>>2=0
      -1>>>2=1073741823
+     1<<6=64
      */
 }
